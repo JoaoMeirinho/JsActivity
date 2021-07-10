4efb2fba16a585e4953ad41d2206572cd362964f
João Meirinho <jao46roberto@gmail.com>
2021-07-10 13:17:55 -0300

    created git and add all files of aula1 to aula14

---
+++ aula03/index.js
@@ -0,0 +1,1 @@
+console.log('Meu nome é "João". Estou aprendendo JavaScript às', 10, 'da manhã');
---
+++ aula07/IMC.js
@@ -0,0 +1,16 @@
+const nome = 'João';
+const idade = 17;
+const peso = 84;
+const altura = 1.80;
+
+let soma = idade + peso;
+let imc;
+
+imc = peso / (altura**2);
+
+console.log('Seu nome é', nome,'e você tem', idade, 'anos.');
+console.log('Seu IMC é de:',imc)
+
+
+
+
