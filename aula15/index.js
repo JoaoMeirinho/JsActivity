869dc4c831ba0335987dfee13322e7a9be9bbf1e
João Meirinho <jao46roberto@gmail.com>
2021-07-17 14:19:59 -0300

    add folder aula15

---
+++ aula15/index.js
@@ -0,0 +1,11 @@
+let num1 = 9.469872;
+// arredondar pra baixo 
+let num2 = Math.floor(num1);
+// arredondar pra cima 
+let num3 = Math.ceil(num1);
+// arredondar para o número mais próximo 
+let num4 = Math.round(num1);
+// console.log(num2, num3);
+
+const aleatorio = Math.round(Math.random() * (10 - 5) + 5) ;
+console.log(aleatorio)
