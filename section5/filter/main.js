489c2cf6195c4bf33aa0479029d42778fc2e7d31
João Meirinho <meirinhojoao2004@gmail.com>
2022-12-28 21:59:33 -0300

    modified filter

--- section5/filter/main.js
+++ section5/filter/main.js
@@ -1,1 +1,23 @@
 const numeros = [5, 50, 80, 1, 2, 3, 4, 5, 8, 7, 11, 15, 22, 27];
+
+const numerosFiltrados = numeros.filter((valor) => valor > 10);
+
+console.log(numerosFiltrados);
+
+const pessoas = [
+  { nome: "Luiz", idade: 62 },
+  { nome: "Maria", idade: 23 },
+  { nome: "Eduardo", idade: 55 },
+  { nome: "Letícia", idade: 19 },
+  { nome: "Rosana", idade: 32 },
+  { nome: "Wallace", idade: 47 },
+];
+
+const pessoasComNomeGrande = pessoas.filter((valor) => valor.nome.length >= 5);
+const pessoasMaisCinquenta = pessoas.filter((valor) => valor.idade > 50);
+const pessoasTerminamComLetraA = pessoas.filter((valor) =>
+  valor.nome.toLowerCase().endsWith("a")
+);
+console.log(pessoasComNomeGrande);
+console.log(pessoasMaisCinquenta);
+console.log(pessoasTerminamComLetraA);
