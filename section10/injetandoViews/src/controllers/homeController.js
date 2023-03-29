ea045edce2697ccf8e19f9735e5295f4413b85e6
João Meirinho <meirinhojoao2004@gmail.com>
2023-03-28 21:26:44 -0300

    inject views

--- section10/injetandoViews/src/controllers/homeController.js
+++ section10/injetandoViews/src/controllers/homeController.js
@@ -2,7 +2,10 @@ const HomeModel = require("../models/HomeModel");
 
 exports.paginaInicial = (req, res) => {
   console.log(req.flash('error'), req.flash('sucess'))
-  res.render("index");
+  res.render("index", {
+    titulo: 'Este será o <span style="color:red;">titulo</span> da página',
+    numeros: [0,1,2,3,4,5,6,7,8,9]
+  });
   return;
 };
 
--- section10/injetandoViews/src/middlewares/middleware.js
+++ section10/injetandoViews/src/middlewares/middleware.js
@@ -1,10 +1,5 @@
 exports.middlewareGlobal = (req, res, next) => {
-  if(req.body.cliente) {
-    req.body.cliente = req.body.cliente.replace('roberto', 'ROBERTO NÃO')
-    console.log()
-    console.log(`vi que vc postou ${req.body.cliente}`)
-    console.log()
-  }
+  res.locals.umaVariavelLocal = 'Este é o valor da variável local';
 
   next();
 }
